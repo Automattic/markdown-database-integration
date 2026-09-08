189c54c998e3fc3949e836a71bdd427545a8d84e
Chris Huber <chubes@extrachill.com>
2026-09-08 18:36:41 -0400

    test: require verified codebox contract

--- tests/smoke-native-codebox-version.php
+++ tests/smoke-native-codebox-version.php
@@ -9,7 +9,7 @@ $binary = sys_get_temp_dir() . '/mdi-wp-codebox-version-' . bin2hex( random_byte
 $failures = array();
 
 try {
-	file_put_contents( $binary, "#!/bin/sh\nprintf '0.24.5\\n'\n" );
+	file_put_contents( $binary, "#!/bin/sh\nprintf '0.26.3\\n'\n" );
 	chmod( $binary, 0755 );
 	try {
 		mdi_native_lifecycle_require_wp_codebox( $binary );
@@ -17,12 +17,12 @@ try {
 		$failures[] = 'minimum supported version was rejected: ' . $error->getMessage();
 	}
 
-	file_put_contents( $binary, "#!/bin/sh\nprintf '0.24.4\\n'\n" );
+	file_put_contents( $binary, "#!/bin/sh\nprintf '0.26.2\\n'\n" );
 	try {
 		mdi_native_lifecycle_require_wp_codebox( $binary );
 		$failures[] = 'unsupported version was accepted';
 	} catch ( RuntimeException $error ) {
-		if ( ! str_contains( $error->getMessage(), '0.24.5 or newer is required; 0.24.4 was found.' ) ) {
+		if ( ! str_contains( $error->getMessage(), '0.26.3 or newer is required; 0.26.2 was found.' ) ) {
 			$failures[] = 'unsupported version error was not actionable: ' . $error->getMessage();
 		}
 	}
